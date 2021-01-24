00dee9fc534d84b6062f395fb025c538edcc7851
Teddy Walter <teddywalter2016@gmail.com>
2021-01-24 22:44:28 +0000

    card component integration

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import './card-list.styles.css'
-
+import { Card } from './../card/card.component'
 export const CardList = props => {
     console.log(props);
     return (
         <div className='card-list'>
             {/* {props.children} */}
             {/* <h2>{props.name}</h2> */}
-            {props.monsters.map( monster => <h1 key={monster.id}>{monster.name}</h1>)}
+            {/* {props.monsters.map( monster => <h1 key={monster.id}>{monster.name}</h1>)} */}
+
+
+            {props.monsters.map(monster => <Card key={monster.id} monster={ monster }/>)}
         </div>
     )
 }
