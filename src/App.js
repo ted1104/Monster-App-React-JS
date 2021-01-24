4e62ef78a16df61938eb114fd00018591e8c96f7
Teddy Walter <teddywalter2016@gmail.com>
2021-01-24 23:33:28 +0000

    search Inbox logic

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ class App extends Component {
   constructor(){
     super();
     this.state = {
-      monsters : []
+      monsters : [],
+      searchField : ''
     }
   }
 
@@ -19,9 +20,19 @@ class App extends Component {
 
   render(){
     // console.log('render')
+    const { monsters, searchField } = this.state;
+    const filteredMonster = monsters.filter( monster => 
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+      )
     return (
       <div className="App">
-         <CardList monsters={this.state.monsters}/> 
+         <input 
+         type="search" 
+         placeholder="search monster" 
+         onChange={ e => this.setState({ searchField : e.target.value }) }
+         />
+         <CardList
+          monsters={filteredMonster}/> 
 
          {/* <CardList name = "teddy">
          { this.state.monsters.map(monster => <h1 key = {monster.id}> { monster.name} </h1>) }
--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './card-list.styles.css'
 import { Card } from './../card/card.component'
 export const CardList = props => {
-    console.log(props);
+    // console.log(props);
     return (
         <div className='card-list'>
             {/* {props.children} */}
